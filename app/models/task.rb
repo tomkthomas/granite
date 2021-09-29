176397e454c582d1b3608567029a7ae049880f4f
Tom Thomas <tomthomas@Toms-MacBook-Pro.local>
2021-09-29 19:39:56 +0530

    Added constraint to make title not nullable

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@
 
 class Task < ApplicationRecord
   validates :title, presence: true
-  validates :title, presence: true, length: { maximum: 50 }
+  # validates :title, presence: true, length: { maximum: 50 }
 end
