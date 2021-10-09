cd6b5ef4064b0aee9b90e9cce20c94080b0ba99d
Tom Thomas <tomthomas@Toms-MacBook-Pro.local>
2021-10-09 23:52:30 +0530

    Added ability to update a task

--- app/javascript/src/apis/tasks.js
+++ app/javascript/src/apis/tasks.js
@@ -3,11 +3,13 @@ import axios from "axios";
 const list = () => axios.get("/tasks");
 const show = slug => axios.get(`/tasks/${slug}`);
 const create = payload => axios.post("/tasks/", payload);
+const update = ({ slug, payload }) => axios.put(`/tasks/${slug}`, payload);
 
 const tasksApi = {
   list,
   show,
-  create
+  create,
+  update
 };
 
 export default tasksApi;
