dd3302c873b26c0f7a25f1f688c199bf6761cd5a
Tom Thomas <tomthomas@Toms-MacBook-Pro.local>
2021-10-13 00:01:23 +0530

    Added Jbuilder template for show action in TasksController

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
+  MAX_TASK_TITLE_LENGTH = 125
   is_sqlite_db = ActiveRecord::Base.connection_db_config.configuration_hash[:adapter] == "sqlite3"
   DB_REGEX_OPERATOR = is_sqlite_db ? "REGEXP" : "~*"
 end
